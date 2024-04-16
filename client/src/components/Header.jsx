4e3968d84ac7654a06122af166a2f23f06de5d41
luka chikashvili <lukachikashvili2020@gmail.com>
2024-04-16 17:22:17 +0400

    add header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='w-full flex items-center justify-between bg-[#525CEB] p-8'>
+       <div className="logo">
+          <h1 className='text-3xl text-white cursor-pointer'>LingoBingo</h1>
+       </div>
+
+       <nav className='text-white flex gap-8 text-xl line'>
+         <Link to = "/" className='line'>მთავარი</Link>
+         <Link to = "/" className='line'>შესახებ</Link>
+         <Link to = "/" className='line'>ლექსიკონი</Link>
+       </nav>
+    </div>
+  )
+}
+
+export default Header
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const cors = require('cors');
+
 
 app.use(express.json());
-
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
