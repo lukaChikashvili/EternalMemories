c98889d9238de920315dfbedce3652a3f4dd5ed1
luka chikashvili <lukachikashvili2020@gmail.com>
2024-04-16 17:04:29 +0400

    add express server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+   console.log(`server running on port ${PORT}`);
+})
