be97c4b081256557e233c42d552c99d06fbee860
luka chikashvili <lukachikashvili2020@gmail.com>
2024-04-16 19:22:58 +0400

    add protected route

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
 import Login from './components/Login';
 import Register from './components/Register';
 
 function App() {
+  const auth = !!localStorage.getItem('token');
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path = "/" element = {<Login />} />
         <Route path = "/register" element = {<Register />} />
-       <Route path = "/profile" element = {<Home />} />
+        <Route path = "/profile" element = {auth ? <Home /> : Navigate({to: '/'})} />  
       </Routes>
     </div>
   );
